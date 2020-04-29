68f86fc2cbc53adcba58d9c954bed42c27a33cab
YanaBrewster <yana.brewster@gmail.com>
2020-04-30 11:33:34 +1200

    cards.php added

--- varieties.php
+++ varieties.php
@@ -21,64 +21,11 @@ require("templates/header.php");
 		</p>
 	</div>
 </div>
+
 <hr class="my-5">
 <div class="row">
-  <div class="col-4">
-    <div class="card border-0 mx-2 my-2">
-      <img src="images/Coffee/A_small_cup_of_coffee.JPG" class="card-img-top" alt="coffee">
-      <div class="card-body">
-        <h3>Long Black</h3>
-        <p class="card-text">A long black is a style of coffee commonly found in Australia and New Zealand. It is similar to an Americano, but with a stronger aroma and taste.A long black is made by pouring a double-shot of espresso or ristretto over hot water. Typically about 100 - 120 milliliters of water is used (3.5 - 4 ounces) but the measurement is considered to be flexible to individual taste.</p>
-      </div>
-    </div>
-  </div>
-  <div class="col-4">
-    <div class="card border-0 mx-2 my-2">
-      <img src="images/Coffee/Cafe-Au-Lait.jpg" class="card-img-top" alt="coffee">
-      <div class="card-body">
-          <h3>Café au lait</h3>
-        <p class="card-text">Café au lait; French for "coffee with milk") is coffee with hot milk added. It differs from white coffee, which is coffee with cold milk or other whiteners added.</p>
-      </div>
-    </div>
-  </div>
-  <div class="col-4">
-    <div class="card border-0 mx-2 my-2">
-      <img src="images/Coffee/caffe_americano.jpg" class="card-img-top" alt="coffee">
-      <div class="card-body">
-          <h3>Caffè Americano</h3>
-        <p class="card-text">Caffè Americano (also known as Americano or American; Italian pronunciation; Spanish: café americano, literally American coffee) is a type of coffee drink prepared by diluting an espresso with hot water, giving it a similar strength to, but different flavor from, traditionally brewed coffee.</p>
-      </div>
-    </div>
-  </div>
-  <div class="col-4">
-    <div class="card border-0 mx-2 my-2">
-      <img src="images/Coffee/galao_kaffee_portugal.jpg" class="card-img-top" alt="coffee">
-      <div class="card-body">
-        <h3>Galão</h3>
-        <p class="card-text">Galão ([ɡaˈlɐ̃õ]) is a hot drink from Portugal made of espresso and foamed milk. Similar to caffè latte or café au lait, it consists of about one quarter coffee and three quarters foamed milk. It is served in a tall glass, as opposed to the smaller garoto that is served in a demitasse. </p>
-      </div>
-    </div>
-  </div>
-  <div class="col-4">
-    <div class="card border-0 mx-2 my-2">
-      <img src="images/Coffee/irish coffee.jpg" class="card-img-top" alt="coffee">
-      <div class="card-body">
-        <h3>Irish Coffee</h3>
-        <p class="card-text">Irish coffee (Irish: caife Gaelach) is a cocktail consisting of hot coffee, Irish whiskey, and sugar, stirred, and topped with cream. The coffee is drunk through the cream.</p>
-      </div>
-    </div>
-  </div>
-  <div class="col-4">
-    <div class="card border-0 mx-2 my-2">
-      <img src="images/Coffee/white-chocolate-peppermint-mocha.jpg" class="card-img-top" alt="coffee">
-      <div class="card-body">
-        <h3>Peppermint White Chocolate Mocha</h3>
-        <p class="card-text">Rich espresso meets white chocolate sauce, peppermint-flavored syrup and steamed milk. Topped with sweet whipped cream and dark chocolate curls.</p>
-      </div>
-    </div>
-  </div>
-</div>
-</main>
+
+  <?php 	require("templates/card.php"); ?>
 
 
 <?php
